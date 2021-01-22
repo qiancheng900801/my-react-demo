5598e5a3e548820ac6281ac34ba8972c1f9274be
qiancheng900801 <qiancheng.edy@gmail.com>
2021-01-22 17:32:01 +0800

    1.22.001

---
+++ react-pro-demo02/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom'
+function Header(params) {
+  return (
+    <div>
+
+    </div>
+  )
+
+}
+export default withRouter(Header)
--- react-pro-demo02/src/components/Home/index.js
+++ react-pro-demo02/src/components/Home/index.js
@@ -4,17 +4,31 @@ import LeftMenu from '../Slider'
 import Welcome from '../../pages/home/home'
 import ProductsList from '../../pages/product/list'
 import ProductsEdit from '../../pages/product/edit'
+import menuList from '../../config/menuConfig'
 import './index.css'
 import { Redirect, Route, Switch } from 'react-router-dom';
 const { Sider, Header, Content, Footer } = Layout
 function Home(props) {
+  console.log(props);
+  function getTitle() {
+    let { pathname } = props.location
+    let title = ''
+    menuList.forEach(item => {
+      if (item.key === pathname) {
+        title = item.title
+      }
+    })
+
+  }
   return (
     <Layout className='home'>
       <Sider style={{ width: '500px' }}>
         <LeftMenu></LeftMenu>
       </Sider>
       <Layout>
-        <Header style={{ height: '80px', background: '#ccc' }}>Header</Header>
+        <Header style={{ height: '80px', background: '#ccc' }}>
+          当前标题：
+        </Header>
         <Content>
           <Switch>
             <Route path='/home' component={Welcome}></Route>
